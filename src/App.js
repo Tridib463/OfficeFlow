f77834322262d4dabc6b7ee60cedc8fc79a8e812
Tridib Chatterjee <tridibtech@gmail.com>
2024-05-28 12:48:52 +0530

    update Title

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import { ROLES } from './config/roles'
 import useTitle from './hooks/useTitle';
 
 function App() {
-  useTitle('Dan D. Repairs')
+  useTitle('OfficeFlow.dev')
 
   return (
     <Routes>
